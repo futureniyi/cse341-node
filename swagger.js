0905a244bda8b68bb04c076a06f66c69aed372b2
futureniyi <futureniyi@yahoo.com>
2026-01-17 13:54:29 +0100

    render fix

--- swagger.js
+++ swagger.js
@@ -1,9 +1,12 @@
 const swaggerAutogen = require('swagger-autogen')();
+const port = process.env.PORT || 3000;
+const host = process.env.SWAGGER_HOST || `localhost:${port}`;
+const scheme = process.env.SWAGGER_SCHEME || 'http';
 
 const doc = {
   info: { title: 'Contacts API', description: 'API documentation' },
-  host: 'localhost:3000',
-  schemes: ['http'],
+  host,
+  schemes: [scheme],
 };
 
 const outputFile = './swagger.json';

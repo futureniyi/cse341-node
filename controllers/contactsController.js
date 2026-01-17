7a54e024ba136df44fce50ef97d48e74d04d12fb
futureniyi <futureniyi@yahoo.com>
2026-01-17 01:11:44 +0100

    adding PUT, POST, DELETE Routes

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
@@ -20,7 +21,70 @@ const getSingleContact = async (req, res) => {
     res.status(200).json(result);
 };
 
+const createContact = async (req, res) => {
+    // Implementation for creating a contact
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+
+    const response = await mongodb
+        .getDatabase()
+        .db()
+        .collection('contacts')
+        .insertOne(contact);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    }
+};
+
+const updateContact = async (req, res) => {
+    // Implementation for updating a contact
+    const contact_id = new ObjectId(req.params.id);
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+
+    const response = await mongodb
+        .getDatabase()
+        .db()
+        .collection('contacts')
+        .replaceOne({ _id: contact_id }, contact);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    }
+}
+
+const deleteContact = async (req, res) => {
+    // Implementation for deleting a contact
+    const contact_id = new ObjectId(req.params.id);
+    const response = await mongodb
+        .getDatabase()
+        .db()
+        .collection('contacts')
+        .deleteOne({ _id: contact_id });
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    }
+}
+
 module.exports = {
     getAllContacts,
-    getSingleContact
+    getSingleContact,
+    createContact,
+    updateContact,
+    deleteContact
 };
--- routes/contacts.js
+++ routes/contacts.js
@@ -5,5 +5,8 @@ const contactsController = require('../controllers/contactsController');
 
 routes.get('/', contactsController.getAllContacts);
 routes.get('/:id', contactsController.getSingleContact);
+routes.post('/', contactsController.createContact);
+routes.put('/:id', contactsController.updateContact);
+routes.delete('/:id', contactsController.deleteContact);
 
 module.exports = routes;
